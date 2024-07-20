f3191b7e138f70c84c6d5faad1e526d05d3a96b1
Steveunouss <stevenloue9@gmail.com>
2024-07-20 03:35:06 -0500

    add routes + old file structure

--- server/server.js
+++ server/server.js
@@ -1,10 +1,1 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const app = express();
 
-// load env variables
-dotenv.config({ path: "./.env" });
-
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => console.log(`listening on port ${port}`));
